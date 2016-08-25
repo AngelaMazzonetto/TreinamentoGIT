1cef7f885110454c70dd4ffcd74eb7f2d686813f
Marcio Nicolau <nicolau.3g@gmail.com>
2016-08-25 11:13:02 -0300

    Add deg2rad function

--- exemplo.c
+++ exemplo.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <math.h>
+
+float deg2rad(float deg) {
+  float rd = (deg * M_PI) / 180.0;
+  return rd;
+}
 
 int main (void)
 {
   printf ("Hello, world!\n");
+
+  printf("The radian of 135 degree is %3.4f\n", deg2rad(135.0));
   return 0;
 }
